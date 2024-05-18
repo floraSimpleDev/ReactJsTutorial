3e090f882640f32760e769752793a17d14f1f4e2
Flora Li <samantha20200604@gmail.com>
2024-05-18 19:40:08 +1000

    basice nav bar design

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,9 +1,42 @@
 import { useState } from "react";
+import search from "./assets/icons/search.svg";
 
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [input, setInput] = useState("");
+
+  const submitCity = () => {
+    setInput("");
+  };
+
+  return (
+    <div className="w-full h-screen text-white px-8">
+      <nav className="w-full p-3 flex justify-between items-center">
+        <h1 className="w-full p-3 flex tracking-wide text-3xl font-bold ">
+          Weather App
+        </h1>
+        <div className="bg-white w-[15rem] overflow-hidden shadow-2xl rounded flex items-center p-2 gap-2">
+          <img
+            src={search}
+            alt="search button"
+            className="w-[1.5rem] h-[1.5rem]"
+          />
+          <input
+            type="text"
+            placeholder="Search city"
+            className="focus:outline-none w-full text-[#212121] text-lg"
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+            onKeyUp={(event) => {
+              if (event.key === "Enter")
+                console.log("submit city"); /* submitCity() */
+            }}
+          />
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
