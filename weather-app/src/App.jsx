f3e7c7d22794165814f3d01b9cc0d2ccec60c973
Flora Li <samantha20200604@gmail.com>
2024-05-21 20:05:12 +1000

    set up layout style of TimeAndLocation section

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from "react";
 import Header from "./component/Header";
+import TimeAndLocation from "./component/TimeAndLocation";
 import "./App.css";
 
 function App() {
   const [input, setInput] = useState("");
 
-  const submitCity = () => {
-    setInput("");
-  };
-
   return (
     <div className="w-full h-screen text-white px-8">
       <Header input={input} setInput={setInput} />
+      <section className="flex justify-center items-center w-full">
+        <TimeAndLocation />
+      </section>
     </div>
   );
 }
--- weather-app/src/main.jsx
+++ weather-app/src/main.jsx
@@ -2,6 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { StateContextProvider } from "./context/index.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
