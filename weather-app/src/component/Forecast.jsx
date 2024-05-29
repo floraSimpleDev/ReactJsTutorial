6d872670008c290a095fc728eca8d1d0a8731191
Flora Li <samantha20200604@gmail.com>
2024-05-29 19:31:29 +1000

    dynamic the forecast paramaters

--- weather-app/src/component/Forecast.jsx
+++ weather-app/src/component/Forecast.jsx
@@ -1,34 +1,33 @@
 import React from "react";
-import rainy from "../assets/icons/rainy.png";
+import Ash from "../assets/icons/Ash.svg";
+import Clear from "../assets/icons/Clear.png";
+import Clouds from "../assets/icons/Clouds.png";
+import Drizzle from "../assets/icons/Drizzle.png";
+import Dust from "../assets/icons/Dust.svg";
+import Fog from "../assets/icons/Fog.png";
+import Haze from "../assets/icons/Haze.svg";
+import Mist from "../assets/icons/Mist.png";
+import Rain from "../assets/icons/Rain.png";
+import Sand from "../assets/icons/Sand.svg";
+import Smoke from "../assets/icons/Smoke.png";
+import Snow from "../assets/icons/Snow.png";
+import Squall from "../assets/icons/Squall.png";
+import Thunderstorm from "../assets/icons/Thunderstorm.png";
+import Tornado from "../assets/icons/Tornado.png";
 
-const Forecast = () => {
-  const datas = [1, 2, 3, 4, 5];
+const Forecast = ({ title, dataset }) => {
   return (
     <div className="mx-6">
       <div className="flex items-center justify-start mt-4">
-        <p className="font-medium uppercase">3 hours step forecast</p>
+        <p className="font-medium uppercase">{title}</p>
       </div>
       <hr className="my-1" />
       <menu className="flex justify-between items-center mt-3 px-3">
-        {datas.map((data, index) => (
+        {dataset.map((data, index) => (
           <li key={index} className="flex-col items-center justify-center">
-            <p className="text-center text-sm">Wed</p>
-            <img src={rainy} className="w-10" alt="weather icon" />
-            <p className="font-medium text-center">12°</p>
-          </li>
-        ))}
-      </menu>
-
-      <div className="flex items-center justify-start mt-4">
-        <p className="font-medium uppercase">3 hours step forecast</p>
-      </div>
-      <hr className="my-1" />
-      <menu className="flex justify-between items-center mt-3 px-3">
-        {datas.map((data, index) => (
-          <li key={index} className="flex-col items-center justify-center">
-            <p className="text-center text-sm">Wed</p>
-            <img src={rainy} className="w-10" alt="weather icon" />
-            <p className="font-medium text-center">12°</p>
+            <p className="text-center text-sm">{data.title}</p>
+            <img src={data.image} className="w-10" alt="weather icon" />
+            <p className="font-medium text-center">{`${data.temp.toFixed()}°`}</p>
           </li>
         ))}
       </menu>
